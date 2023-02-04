b99d74edbfbaf6934d91d3f0929642e675e52f2f
optionsx <fake@protonmail.com>
2023-02-05 02:18:41 +0300

    reforms

--- test.ts
+++ test.ts
@@ -1,6 +1,6 @@
 import {
   decode,
   encode,
-} from "https://raw.githubusercontent.com/optionsx/GPT-3-Encoder/master/src/Encoder.ts";
+} from "https://raw.githubusercontent.com/optionsx/GPT-3-Encoder/master/src/mod.ts";
 
 console.log(encode("Hello World!"));
