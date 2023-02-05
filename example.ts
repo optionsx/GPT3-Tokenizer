72893a9a752ba971c0039833baa784f2a93aeaae
optionsx <fake@protonmail.com>
2023-02-05 14:00:26 +0300

    reforms

--- example.ts
+++ example.ts
@@ -3,16 +3,13 @@ import {
   encode,
 } from "https://raw.githubusercontent.com/optionsx/GPT3-Tokenizer/master/src/mod.ts";
 
-const str = "tokenize this, biji hevall, constructor";
+const tokenizeThis = "biji hevall, constructor";
 console.log();
 // const encoded = encode(str);
-const encoded = encode(str);
+const encoded = encode(tokenizeThis);
 console.log("tokenized: ", encoded);
 
 console.log("We can look at each token and what it represents");
-// for (const token of encoded) {
-//   console.log({ token, string: decode([token]) });
-// }
 for (const token of encoded) {
   console.log({ token, string: decode([token]) });
 }
